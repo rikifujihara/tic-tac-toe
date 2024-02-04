400acfee1a4a0f83bac2c4b478505e9e014b941e
Riki Fujihara <riki.fujihara@au.ey.com>
2024-02-04 18:35:54 +1100

    gameover logic + refactoring

---
+++ src/components/Player.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+export default function Player({
+  initialName,
+  symbol,
+  isActive,
+  onChangeName,
+}) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleClick() {
+    setIsEditing((editing) => !editing);
+
+    if (isEditing) {
+      onChangeName(symbol, playerName);
+    }
+  }
+
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  let editableName = <span className="player-name">{playerName}</span>;
+
+  if (isEditing) {
+    editableName = (
+      <input type="text" required value={playerName} onChange={handleChange} />
+    );
+  }
+
+  return (
+    <li className={isActive ? "active" : undefined}>
+      <span className="player">{editableName}</span>
+      <button onClick={handleClick}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
+}
